3a8917b4a4ddefd0e94bae14848aebf843246f58
zjzzpp <38099425+zjzzpp@users.noreply.github.com>
2024-08-15 10:47:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.zjzpp.org": dockerHub,
+  "quay.zjzpp.org": "https://quay.io",
+  "gcr.zjzpp.org": "https://gcr.io",
+  "k8s-gcr.zjzpp.org": "https://k8s.gcr.io",
+  "k8s.zjzpp.org": "https://registry.k8s.io",
+  "ghcr.zjzpp.org": "https://ghcr.io",
+  "cloudsmith.zjzpp.org": "https://docker.cloudsmith.io",
+  "ecr.zjzpp.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.zjzpp.org": dockerHub,
 };
 
 function routeByHosts(host) {
